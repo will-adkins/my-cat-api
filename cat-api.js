1c4dcbffa6f9616c77292bb6df337bcb6aeff3e5
tripott <tripott@gmail.com>
2018-06-13 09:21:58 -0400

    listening

--- cat-api.js
+++ cat-api.js
@@ -5,3 +5,14 @@ const app = express()
 app.get('/', function(req, res) {
   res.send('Welcome to the CATS api, meow.')
 })
+
+app.listen(
+  process.env.PORT || 5555,
+  process.env.HOST || '127.0.0.1',
+  function() {
+    console.log(
+      'API is up, meow',
+      `http://${process.env.HOST || '127.0.0.1'}:${process.env.PORT || 5555}`
+    )
+  }
+)
